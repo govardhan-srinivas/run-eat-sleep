fc6008cc0ea1e9b29400c0da5dcda53a414a3599
govardhan <govardhan.s@mobinius.com>
2020-04-30 20:29:08 +0530

    fix: updated with some css animations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'parallax'`, () => {
+  it(`should have as title 'run-eat-sleep'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('parallax');
+    expect(app.title).toEqual('run-eat-sleep');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to parallax!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to run-eat-sleep!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   config: any;
   fullpage_api: any;
+  activePage: number;
 
   constructor() {
-
     // for more details on config options please visit fullPage.js docs
     this.config = {
-
       // fullpage options
       licenseKey: null,
-      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-      menu: '#menu',
-      slidesNavigation:true,
-      // fullpage callbacks
-      afterResize: () => {
-        console.log("After resize");
+      anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage"],
+      menu: "#menu",
+      slidesNavigation: true,
+      continuousVertical: true,
+      afterLoad: (origin, destination) => {
+        console.log(destination);
+        this.activePage = destination.index;
       },
-      afterLoad: (origin, destination, direction) => {
-        console.log(origin.index);
-      }
     };
   }
 
   getRef(fullPageRef) {
     this.fullpage_api = fullPageRef;
   }
-
 }
